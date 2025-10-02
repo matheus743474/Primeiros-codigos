c54eb399064e66f9ad54185d0224a43a632a2d6c
Matheus <matheussss6479@gmail.com>
2025-10-01 21:40:01 -0300

    Correção

--- Execícios python/056.py
+++ Execícios python/056.py
@@ -1,8 +1,19 @@
-media = 0
+todas_idade = 0
+maior_idade = 0
 homem_velho = ""
+mulher = 0
 for x in range(4):
-    nome = str(input("Digite o nome: "))
-    idade = int("Digite a idade: ")
-    sexo = str("Digite o sexo: ")
-    if idade > homem_velho:
-        homem_velho = idade
+    nome = str(input("Digite o nome: ")).strip()
+    idade = int(input("Digite a idade: "))
+    sexo = str(input("Digite o sexo: ")).strip().lower()
+    todas_idade += idade
+    if sexo == "feminino" and idade < 20:
+        mulher += 1
+    if idade > maior_idade and sexo == "masculino":
+        homem_velho = nome
+        maior_idade = idade
+    print("")
+media = todas_idade / 4
+print(f"A média de idade do grupo é {media}")
+print(f"O homem mais velho é {homem_velho}")
+print(f"Tem {mulher} mulheres com menos de 20 anos")
